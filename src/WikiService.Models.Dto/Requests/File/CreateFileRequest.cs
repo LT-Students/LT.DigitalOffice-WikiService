b6a48e5d01a5134270e9f4afe55e8f20fb0269ae
milfoilsara <nutovna25@gmail.com>
2022-05-18 14:28:52 +0300

    added required to file content

--- src/WikiService.Models.Dto/Requests/File/CreateFileRequest.cs
+++ src/WikiService.Models.Dto/Requests/File/CreateFileRequest.cs
@@ -11,6 +11,7 @@ namespace LT.DigitalOffice.WikiService.Models.Dto.Models
   {
     [Required]
     public string Name { get; set; }
+    [Required]
     public string Content { get; set; }
     [Required]
     public string Extension { get; set; }
