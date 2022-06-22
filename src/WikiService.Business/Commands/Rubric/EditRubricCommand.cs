458a7913b1244b1ecadcca7c9937482501b609c3
AnnPostnikova <postnikova170@yandex.ru>
2022-06-22 16:48:08 +0300

    fix

--- src/WikiService.Business/Commands/Rubric/EditRubricCommand.cs
+++ src/WikiService.Business/Commands/Rubric/EditRubricCommand.cs
@@ -14,7 +14,6 @@ using LT.DigitalOffice.WikiService.Mappers.PatchDocument.Interfaces;
 using LT.DigitalOffice.WikiService.Models.Db;
 using LT.DigitalOffice.WikiService.Models.Dto.Requests.Rubric;
 using LT.DigitalOffice.WikiService.Validation.Rubric.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 
 namespace LT.DigitalOffice.WikiService.Business.Commands.Rubric
@@ -25,7 +24,6 @@ namespace LT.DigitalOffice.WikiService.Business.Commands.Rubric
     private readonly IAccessValidator _accessValidator;
     private readonly IPatchDbRubricMapper _mapper;
     private readonly IRubricRepository _rubricRepository;
-    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IResponseCreator _responseCreator;
 
     public EditRubricCommand(
@@ -33,14 +31,12 @@ namespace LT.DigitalOffice.WikiService.Business.Commands.Rubric
       IAccessValidator accessValidator,
       IPatchDbRubricMapper mapper,
       IRubricRepository rubricRepository,
-      IHttpContextAccessor httpContextAccessor,
       IResponseCreator responseCreator)
     {
       _validator = validator;
       _accessValidator = accessValidator;
       _mapper = mapper;
       _rubricRepository = rubricRepository;
-      _httpContextAccessor = httpContextAccessor;
       _responseCreator = responseCreator;
     }
 
