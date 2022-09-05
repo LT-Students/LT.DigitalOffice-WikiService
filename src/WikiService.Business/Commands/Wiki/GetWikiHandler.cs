8e81d5915a7feb0c4539632f1beb1c1b98520dcb
Vladimir Liamin <LyaminVA@yandex.ru>
2022-09-05 21:47:54 +0300

    fixes

--- src/WikiService.Business/Commands/Wiki/GetWikiHandler.cs
+++ src/WikiService.Business/Commands/Wiki/GetWikiHandler.cs
@@ -4,7 +4,6 @@ using LT.DigitalOffice.WikiService.Models.Dto.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
--- src/WikiService.Business/Commands/Wiki/RubricData.cs
+++ src/WikiService.Business/Commands/Wiki/RubricData.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LT.DigitalOffice.WikiService.Models.Dto.Responses.Wiki
+namespace LT.DigitalOffice.WikiService.Business.Commands.Wiki
 {
-  public record WikiTreeResponse
+  public record RubricData
   {
     public Guid Id { get; set; }
     public string Name { get; set; }
     public bool IsActive { get; set; }
+    public Guid? ParentId { get; set; }
     public List<string> ArticlesNames { get; set; }
-    public List<WikiTreeResponse> Children { get; set; }
+    public List<RubricData> Children { get; set; }
   }
 }
