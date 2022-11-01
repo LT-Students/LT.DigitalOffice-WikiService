75a689063be367ad27732a2df52134b5b3065223
Vladimir Liamin <LyaminVA@yandex.ru>
2022-11-01 16:39:50 +0300

    added check rights to wikiTree controller

--- src/WikiService/Controllers/WikiTreeController.cs
+++ src/WikiService/Controllers/WikiTreeController.cs
@@ -1,4 +1,6 @@
-﻿using LT.DigitalOffice.WikiService.Business.Commands.Wiki;
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Interfaces;
+using LT.DigitalOffice.Kernel.Constants;
+using LT.DigitalOffice.WikiService.Business.Commands.Wiki;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
@@ -11,10 +13,14 @@ namespace LT.DigitalOffice.WikiService.Controllers
   public class WikiTreeController : ControllerBase
   {
     private readonly IMediator _mediator;
+    private readonly IAccessValidator _accessValidator;
 
-    public WikiTreeController(IMediator mediator)
+    public WikiTreeController(
+      IMediator mediator,
+      IAccessValidator accessValidator)
     {
       _mediator = mediator;
+      _accessValidator = accessValidator;
     }
 
     [HttpGet("get")]
@@ -22,6 +28,12 @@ namespace LT.DigitalOffice.WikiService.Controllers
       [FromQuery] GetWikiFilter request,
       CancellationToken ct)
     {
+      if (request.includeDeactivated
+        && !await _accessValidator.HasRightsAsync(Rights.AddEditRemoveWiki))
+      {
+        return StatusCode(403);
+      }
+
       return Ok(await _mediator.Send(request, ct));
     }
   }
