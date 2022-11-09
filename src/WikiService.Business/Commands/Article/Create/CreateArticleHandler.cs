0dccbc6497aab6511c7fb194c18aa2c6de446eba
VLiamin <43515136+VLiamin@users.noreply.github.com>
2022-11-09 13:31:29 +0300

    clean memory in create endpoints (#22)

--- src/WikiService.Business/Commands/Article/Create/CreateArticleHandler.cs
+++ src/WikiService.Business/Commands/Article/Create/CreateArticleHandler.cs
@@ -1,9 +1,11 @@
 ﻿using LT.DigitalOffice.Kernel.Extensions;
+using LT.DigitalOffice.WikiService.Business.Commands.Wiki;
 using LT.DigitalOffice.WikiService.Data.Provider;
 using LT.DigitalOffice.WikiService.Models.Db;
 using LT.DigitalOffice.WikiService.Models.Dto.Requests.Article;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,13 +16,16 @@ namespace LT.DigitalOffice.WikiService.Business.Commands.Article.Create
   {
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IDataProvider _provider;
+    private readonly IMemoryCache _cache;
 
     public CreateArticleHandler(
       IHttpContextAccessor httpContextAccessor,
-      IDataProvider provider)
+      IDataProvider provider,
+      IMemoryCache cache)
     {
       _httpContextAccessor = httpContextAccessor;
       _provider = provider;
+      _cache = cache;
     }
 
     public async Task<Guid?> Handle(CreateArticleRequest request, CancellationToken ct)
@@ -38,6 +43,9 @@ namespace LT.DigitalOffice.WikiService.Business.Commands.Article.Create
 
       await _provider.Articles.AddAsync(article, ct);
       await _provider.SaveAsync();
+
+      _cache.Remove(CacheKeys.WikiTreeWithDeactivated);
+      _cache.Remove(CacheKeys.WikiTreeWithoutDeactivated);
 
       return article.Id;
     }
